e8af8723b46d934d7c76f850f729a46741f1af58
Bojidar Tenev <bojidar_tenev@yahoo.com>
2025-07-02 21:38:09 +0100

    adding a close with grace

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -1,3 +1,4 @@
+import closeWithGrace from 'close-with-grace';
 import { buildApp } from "./app.js";
 const opts = {};
 if (process.stdout.isTTY) {
@@ -12,6 +13,19 @@ else {
     opts.logger = true;
 }
 const app = await buildApp(opts);
+closeWithGrace(async ({ signal, err, manual }) => {
+    if (err) {
+        app.log.error({ err }, 'server closing with error');
+    }
+    else {
+        app.log.info(`${signal} received, server closing`);
+    }
+    await app.close();
+});
+// error example
+// setTimeout(() => {
+//     throw new Error('test')
+// }, 1000)
 app.listen({ port: 3000, host: '0.0.0.0' }, (err, address) => {
     if (err) {
         console.error(err);
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,4 +1,4 @@
-import fastify from "fastify";
+import closeWithGrace from 'close-with-grace'
 import { buildApp } from "./app.js";
 
 interface opts {
@@ -24,6 +24,19 @@ if (process.stdout.isTTY) {
 
 const app = await buildApp(opts)
 
+closeWithGrace(async ({signal, err, manual}) => {
+    if(err) {
+        app.log.error({err}, 'server closing with error')
+    } else {
+        app.log.info(`${signal} received, server closing`)
+    }
+    await app.close()
+})
+
+// error example
+// setTimeout(() => {
+//     throw new Error('test')
+// }, 1000)
 
 app.listen({ port: 3000, host: '0.0.0.0' }, (err, address: string) => {
     if(err) {
