412cda63a4d2b3fa95562eed21a2e7f9c56c2d5d
Bojidar Tenev <bojidar_tenev@yahoo.com>
2025-07-05 11:10:28 +0100

    datasource config for db/orm

---
+++ backend/src/db/data-source.ts
@@ -0,0 +1,22 @@
+import "reflect-metadata";
+import { DataSource } from "typeorm";
+import { User } from "./entities/User.js";
+
+const AppDataSource = new DataSource({
+  type: "postgres",
+  host: process.env.DB_HOST || "localhost",
+  port: Number(process.env.DB_PORT) || 5432,
+  username: process.env.DB_USERNAME || "user1",
+  password: process.env.DB_PASSWORD || "12345",
+  database: process.env.DB_NAME || "db1",
+  synchronize: false, // use migrations instead!
+  logging: true,
+  // entities: ["src/db/entities/**/*.js"],
+  entities: [
+    User,
+  ],
+
+  migrations: ["src/db/migrations/**/*.js"],
+});
+
+export default AppDataSource
