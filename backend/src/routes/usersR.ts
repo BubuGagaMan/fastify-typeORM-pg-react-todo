5447b4b1de4eb279f9f3be39ef03991d74cd35d0
Bojidar Tenev <bojidar_tenev@yahoo.com>
2025-07-05 11:11:11 +0100

    add schema to route

--- backend/src/routes/usersR.ts
+++ backend/src/routes/usersR.ts
@@ -2,11 +2,23 @@ import { FastifyInstance } from "fastify";
 import usersC from "../controllers/usersC.js";
 
 export default async function usersR(app: FastifyInstance) {
+  const postSchema = {
+    schema: {
+      body: {
+        type: "object",
+        properties: {
+          username: { type: "string" },
+          email: { type: "string" },
+          password: { type: "string" },
+        },
+        required: ["username", "password", "email"],
+      },
+    },
+  };
 
   app.get("/users", usersC.getAll);
   app.get("/users/:id", usersC.getById);
   app.post("/users", usersC.create);
-  app.put("/users/:id", usersC.updateById);
+  app.put("/users/:id", postSchema, usersC.updateById);
   app.delete("/users/:id", usersC.deleteById);
-
 }
