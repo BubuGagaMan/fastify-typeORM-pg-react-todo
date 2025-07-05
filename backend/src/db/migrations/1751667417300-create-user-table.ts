496ef6a5d7176f557b160eb90b2e1c0306f92753
Bojidar Tenev <bojidar_tenev@yahoo.com>
2025-07-05 14:08:21 +0100

    alter user creation table to have a uuid id pk (reverted beforehand and then ran again)

--- backend/src/db/migrations/1751667417300-create-user-table.ts
+++ backend/src/db/migrations/1751667417300-create-user-table.ts
@@ -4,7 +4,7 @@ export class CreateUserTable1751667417300 implements MigrationInterface {
     name = 'CreateUserTable1751667417300'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`CREATE TABLE "user" ("id" SERIAL NOT NULL, "username" character varying NOT NULL, "email" character varying NOT NULL, "password" character varying NOT NULL, CONSTRAINT "PK_cace4a159ff9f2512dd42373760" PRIMARY KEY ("id"))`);
+        await queryRunner.query(`CREATE TABLE "user" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "username" character varying NOT NULL, "email" character varying NOT NULL, "password" character varying NOT NULL, CONSTRAINT "PK_cace4a159ff9f2512dd42373760" PRIMARY KEY ("id"))`);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
